c4567bf16787bbeb61c93897c00169aa47e894b2
Robbie Elias <robbieelias1@gmail.com>
2016-03-31 19:46:58 -0400

    Added bootstrap

--- Web/index.php
+++ Web/index.php
@@ -1,28 +1,41 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Home</title>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-</head>
-<body>
-
 <?php
 
 # include the config file
-require "config.php";
+require "./includes/config.php";
 
 # include the database class
-require "Db.class.php";
+require "./includes/Db.class.php";
 
 # create a database object
 $db = new Db();
 
 $test = $db->query('SELECT id, name FROM test');
 
-var_dump($test);
-
 ?>
 
-
 </body>
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Rotten Potatoes</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <p>Ready from the database works!!!</p>
+    <p><?php var_dump($test); ?></p>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
 </html>
