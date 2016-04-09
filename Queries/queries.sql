d39e8dc59b21d508a9b710c8736afee2ba1596e1
isaacshannon <isaac.shannon@gmail.com>
2016-04-09 17:25:11 -0400

    queries f-l

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -40,7 +40,6 @@ SELECT D.name, S.name, T.description
 -- contained in your database. Display this information together with the details of the director(s)
 -- and the studio(s) that s(he) worked with.
 
-
 SELECT actorID, name, dateOfBirth
 	FROM actor
 	WHERE actorID = (SELECT actorID
@@ -74,34 +73,111 @@ SELECT A.name
 		LIMIT 2
 		) T,
 		actor A
-	WHERE A.actorID = T.ID
-
+	WHERE A.actorID = T.ID;
 	
 -- Ratings of movies
 -- #################################
 -- f. Find the names of the ten movies with the highest overall ratings in your database.
--- 
+
+SELECT M.movieID, M.name
+	FROM(SELECT T.ID AS ID
+		FROM (SELECT DISTINCT M.movieID AS ID, W.rating AS R
+			FROM watches W, movie M
+			WHERE M.movieID = W.movieID
+			AND W.rating NOTNULL) T	
+		GROUP BY T.ID
+		ORDER BY SUM(T.R) DESC
+		limit 10) T,
+		movie M
+	WHERE T.ID = M.movieID;
+	
 -- g. Find the movie(s) with the highest overall rating in your database. Display all the movie details,
 -- together with the topics (tags) associated with it.
--- 
+
+SELECT M.movieID, M.name, TC.description
+	FROM(SELECT T.ID AS ID
+		FROM (SELECT DISTINCT M.movieID AS ID, W.rating AS R
+			FROM watches W, movie M
+			WHERE M.movieID = W.movieID
+			AND W.rating NOTNULL) T	
+		GROUP BY T.ID
+		ORDER BY SUM(T.R) DESC
+		limit 1) T,
+		movie M,
+		topics TC,
+		movieTopics MT
+	WHERE T.ID = M.movieID
+	AND M.movieID = MT.movieID
+	AND MT.topicID = TC.topicID;
+
+
 -- h. Find the total number of rating for each movie, for each user. That is, the data should be
 -- grouped by the movie, the specific users and the numeric ratings they have received.
--- 
+
+SELECT U.firstName, U.lastName, M.name, W.rating
+	FROM movieUser U, movie M, watches W
+	WHERE W.userID = U.userID
+	AND M.movieID = W.movieID;
+
 -- i. Display the details of the movies that have not been rated since January 2016.
--- 
+
+SELECT M.name, M.movieID, M.dateReleased
+	FROM movie M
+	WHERE M.movieID NOT IN (SELECT W.movieID as MID
+					FROM watches W
+					WHERE W.dateWatched > '2016-04-08');
+	
 -- j. Find the names, release dates and the names of the directors of the movies that obtained rating
 -- that is lower than any rating given by user X. Order your results by the dates of the ratings.
 -- (Here, X refers to any user of your choice.)
--- 
+SELECT DISTINCT M.movieID, M.name, M.dateReleased, DTOR.name
+	FROM(SELECT W.rating as R
+		FROM watches W, movieUser U
+		WHERE W.userID = 70
+		AND W.rating NOTNULL
+		ORDER BY W.rating DESC
+		LIMIT 1) T, movie M, watches W, director DTOR, directs DECTS
+	WHERE M.movieID = W.movieID
+	AND W.rating NOTNULL
+	AND W.rating < T.R
+	AND M.movieID = DECTS.movieID
+	AND DECTS.directorID = DTOR.directorID;
+
 -- k. List the details of the Type Y movie that obtained the highest rating. Display the movie name
 -- together with the name(s) of the rater(s) who gave these ratings. (Here, Type Y refers to any
 -- movie type of your choice, e.g. Horror or Romance.)  
--- 
+SELECT U.firstName, U.lastName, T.MNAME
+	FROM(SELECT DISTINCT M.movieID as MID, M.name as MNAME, M.dateReleased, T.description
+		FROM movie M, movieTopics MT, topics T, watches W
+		WHERE M.movieID = MT.movieID
+		AND MT.topicID = 15
+		AND T.topicID = 15
+		AND MT.movieID = M.movieID
+		AND W.movieID = M.movieID 
+		AND W.rating >= (SELECT DISTINCT W.rating
+					FROM watches W
+					WHERE W.rating NOTNULL
+					ORDER BY W.rating DESC
+					LIMIT 1)
+		limit 1) T, movieUser U, watches W
+	WHERE W.movieID = T.MID
+	AND W.userID = U.userID;
+
 -- l. Provide a query to determine whether Type Y movies are “more popular” than other movies.  
 -- (Here, Type Y refers to any movie type of your choice, e.g. Nature.) Yes, this query is open to
 -- your own interpretation!
--- Users and their ratings
--- 
+SELECT TC.description, T.S
+	FROM(SELECT T.TID AS TID, sum(T.R) AS S
+		FROM(SELECT T.topicID AS TID, W.rating AS R
+			FROM topics T, watches W, movieTopics MT
+			WHERE MT.movieID = W.movieID
+			AND MT.topicID = T.topicID
+			AND W.rating NOTNULL) T
+		GROUP BY TID
+		ORDER BY SUM(T.R) DESC) T,topics TC
+	WHERE TC.topicID = T.TID
+	ORDER BY T.S DESC
+	
 -- m. Find the names, join‐date and profiling information (age‐range, gender, and so on) of the users
 -- that give the highest overall ratings. Display this information together with the names of the
 -- movies and the dates the ratings were done.
