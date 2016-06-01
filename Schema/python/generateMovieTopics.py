644ef1c334e4157b644cecbcd7a8749833ab79a5
Robbie Elias <robbieelias1@gmail.com>
2016-06-01 10:33:08 -0400

    Update generateMovieTopics.py

--- Schema/python/generateMovieTopics.py
+++ Schema/python/generateMovieTopics.py
@@ -44,7 +44,7 @@ def createMovieTopics(movieID):
 
 
 # connect to the project database
-conn = psycopg2.connect(database="CSI2132", user="csi2132", password="csi2132", host="159.203.44.157", port="5432")
+conn = psycopg2.connect(database="CSI2132", user="csi2132", password="csi2132", host="127.0.0.1", port="5432")
 print "Opened database successfully"
 
 #fill the database for a movie
